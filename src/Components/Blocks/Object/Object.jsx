dce60ebd4799af72aa2290d0fa019a10e176aeb8
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-04-04 15:32:28 +0300

    Update

---
+++ src/Components/Blocks/Object/Object.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classes from './Object.module.css';
+
+function Object({ children, ...props }) {
+    function truncateString(str, maxLength) {
+        if (str.length > maxLength) {
+            return str.substring(0, maxLength) + '...';
+        }
+        return str;
+    }
+
+    return (
+        <>
+            <div className={classes.objects_item}>
+                <div className={classes.objects_item__img}>
+                    <img src={`/${props.img}`} alt="" />
+                </div>
+                <div className={classes.objects_item__bottom}>
+                    <div className={classes.objects_item__title}>
+                        {truncateString(props.title, 33)}
+                    </div>
+                    <div className={classes.objects_item__price}>
+                        <img src={`/${props.priceImg}`} alt="" />
+                        {props.price}
+                    </div>
+                    <div className={classes.objects_item__button}>Подробнее</div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Object;
--- src/Components/Blocks/Tours/Tours.jsx
+++ src/Components/Blocks/Tours/Tours.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import classes from './Tours.module.css';
+import Object from "../Object/Object";
 
 function Tours({ children, ...props }) {
-    function truncateString(str, maxLength) {
-        if (str.length > maxLength) {
-            return str.substring(0, maxLength) + '...';
-        }
-        return str;
-    }
+    let objects = [
+        {
+            img : "object_1.png",
+            title : "«Нoвогодняя сказка Кавказа»",
+            price : "От 10000 р.",
+            priceImg : "priceImg.png",
+        },
+        {
+            img : "object_2.png",
+            title : "«Нoвогодние каникулы на Кавказе»",
+            price : "От 10000 р.",
+            priceImg : "priceImg.png",
+        },
+        {
+            img : "object_3.png",
+            title : "«Нoвогоднее путешествие по Кавказу»",
+            price : "От 10000 р.",
+            priceImg : "priceImg.png",
+        },
+    ]
+
     return (
         <>
             <div className={classes.fullBlock}>
                 <div className={classes.filter}>
                     <select>
-                        <option value="" disabled defaultValue>Город</option>
+                        <option value="" disabled selected>Город</option>
                         <option value="Черкесск">Черкесск</option>
                         <option value="Карачаевск">Карачаевск</option>
                         <option value="Архыз">Архыз</option>
@@ -21,7 +37,7 @@ function Tours({ children, ...props }) {
                         <option value="Теберда">Теберда</option>
                     </select>
                     <select>
-                        <option value="" disabled defaultValue>Категория</option>
+                        <option value="" disabled selected>Категория</option>
                         <option value="Детские">Детские</option>
                         <option value="Гастрономические">Гастрономические</option>
                         <option value="Конные">Конные</option>
@@ -30,58 +46,18 @@ function Tours({ children, ...props }) {
                     </select>
                     <input type="date" />
                     <select>
-                        <option value="Групповая поездка" defaultValue>Групповая поездка</option>
+                        <option value="Групповая поездка" selected>Групповая поездка</option>
                         <option value="Индивидуальная поездка">Индивидуальная поездка</option>
                         <option value="VIP">VIP</option>
                     </select>
                 </div>
 
                 <div className={classes.objects}>
-                    <div className={classes.objects_item}>
-                        <div className={classes.objects_item__img}>
-                            <img src="/object_1.png" alt="" />
-                        </div>
-                        <div className={classes.objects_item__bottom}>
-                            <div className={classes.objects_item__title}>
-                                {truncateString("«Нoвогодняя сказка Кавказа»", 35)}
-                            </div>
-                            <div className={classes.objects_item__price}>
-                                <img src="/priceImg.png" alt="" />
-                                От 10000 р.
-                            </div>
-                            <div className={classes.objects_item__button}>Подробнее</div>
-                        </div>
-                    </div>
-                    <div className={classes.objects_item}>
-                        <div className={classes.objects_item__img}>
-                            <img src="/object_2.png" alt="" />
-                        </div>
-                        <div className={classes.objects_item__bottom}>
-                            <div className={classes.objects_item__title}>
-                                {truncateString("«Нoвогодние каникулы на Кавказе»", 35)}
-                            </div>
-                            <div className={classes.objects_item__price}>
-                                <img src="/priceImg.png" alt="" />
-                                От 10000 р.
-                            </div>
-                            <div className={classes.objects_item__button}>Подробнее</div>
-                        </div>
-                    </div>
-                    <div className={classes.objects_item}>
-                        <div className={classes.objects_item__img}>
-                            <img src="/object_3.png" alt="" />
-                        </div>
-                        <div className={classes.objects_item__bottom}>
-                            <div className={classes.objects_item__title}>
-                                {truncateString("«Нoвогоднее путешествие по Кавказу»", 34)}
-                            </div>
-                            <div className={classes.objects_item__price}>
-                                <img src="/priceImg.png" alt="" />
-                                От 10000 р.
-                            </div>
-                            <div className={classes.objects_item__button}>Подробнее</div>
-                        </div>
-                    </div>
+                    {
+                        objects.map((item, index) => (
+                            <Object key={index} img={item.img} title={item.title} priceImg={item.priceImg} price={item.price} />
+                        ))
+                    }
                 </div>
             </div>
         </>
