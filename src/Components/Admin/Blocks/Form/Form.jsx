55a07c9723297cca146131d205ef9fdfaf8afdd8
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-04-24 19:45:12 +0300

    Update add region

--- src/Components/Admin/Blocks/Form/Form.jsx
+++ src/Components/Admin/Blocks/Form/Form.jsx
@@ -4,64 +4,67 @@ import axios from 'axios';
 import classes from './Form.module.css';
 
 function Form({ children, ...props }) {
-    const [formData, setFormData] = useState({
+    const [form, setForm] = useState({
         title: '',
         description: '',
-        icon: null,
-        backgroundImg: null
+        iconPath: null,
+        backgroundImgPath: null
     });
 
     const handleChange = (event) => {
         const { name, type, files, value } = event.target;
         if (type === 'file') {
-            setFormData(prevState => ({
+            setForm(prevState => ({
                 ...prevState,
                 [name]: files[0]
             }));
         } else {
-            setFormData(prevState => ({
+            setForm(prevState => ({
                 ...prevState,
                 [name]: value
             }));
         }
+
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
-        const data = new FormData();
-        for (const key in formData) {
-            data.append(key, formData[key]);
+        const formData = new FormData();
+    
+        formData.append('title', form.title);
+        formData.append('description', form.description);
+        formData.append('iconPath', form.iconPath); 
+        formData.append('backgroundImgPath', form.backgroundImgPath);
+    
+        try {
+            const response = await axios({
+                method: 'post',
+                url: 'http://localhost:5002/api/addRegion',
+                data: formData,
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
+            console.log(response); 
+        } catch (error) {
+            console.error(error);
         }
-
-        console.log(data);
-
-        // try {
-        //     const response = await axios.post('https://your-backend-url.com/regions', data, {
-        //         headers: {
-        //             'Content-Type': 'multipart/form-data'
-        //         }
-        //     });
-        //     console.log('Region added:', response.data);
-        // } catch (error) {
-        //     console.error('Failed to upload region:', error);
-        // }
     };
+    
+
 
     return (
         <>
             <form className={classes.addData_form} onSubmit={handleSubmit}>
                 <label>Введите название региона</label>
-                <input name="title" type="text" placeholder="Название" value={formData.title} onChange={handleChange} />
+                <input name="title" type="text" placeholder="Название" value={form.title} onChange={handleChange} />
 
                 <label>Добавить описание</label>
-                <textarea name="description" placeholder="Описание" value={formData.description} onChange={handleChange} />
+                <textarea name="description" placeholder="Описание" value={form.description} onChange={handleChange} />
 
                 <label>Загрузите фоновое фото региона</label>
-                <input type="file" name="icon" className={classes.noBorder} onChange={handleChange} />
+                <input type="file" name="iconPath" className={classes.noBorder} onChange={handleChange} />
 
                 <label>Загрузите фото для обложки региона</label>
-                <input type="file" name="backgroundImg" className={classes.noBorder} onChange={handleChange} />
+                <input type="file" name="backgroundImgPath" className={classes.noBorder} onChange={handleChange} />
 
                 <button type="submit">Добавить регион</button>
             </form>
