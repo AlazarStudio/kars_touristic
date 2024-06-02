dabb4b93add11a0450656e0aaef8d2493118df25
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-06-02 18:37:31 +0300

    Update

--- src/serverConfig.js
+++ src/serverConfig.js
@@ -1,6 +1,6 @@
 function serverConfig() {
-    const server = 'http://localhost:5002';
-    // const server = 'http://5.35.91.96:5002';
+    // const server = 'http://localhost:5002';
+    const server = 'http://5.35.91.96:5002';
     return server;
 }
 
