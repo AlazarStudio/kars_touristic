a3029ed7338a81ef14b766a51ac42a630bc56e9a
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-06-07 22:15:37 +0300

    Update

--- src/Components/Blocks/Tours/Tours.jsx
+++ src/Components/Blocks/Tours/Tours.jsx
@@ -78,11 +78,11 @@ function Tours({ children, requestType, pageName, tableName, similar, ...props }
                         <CenterBlock>
                             <WidthBlock>
                                 <div className={classes.centerPosition}>
+                                    <div className={classes.tour_topInfo__left___title}>
+                                        {tour.tourTitle}
+                                    </div>
                                     <div className={classes.tour_topInfo}>
                                         <div className={classes.tour_topInfo__left}>
-                                            <div className={classes.tour_topInfo__left___title}>
-                                                {tour.tourTitle}
-                                            </div>
                                             <div className={classes.tour_topInfo__left___items}>
                                                 <div className={classes.tour_topInfo__left___items____element}>
                                                     <div className={classes.tour_topInfo__left___items____element_____title}>Cпособ передвижения:</div>
@@ -126,44 +126,46 @@ function Tours({ children, requestType, pageName, tableName, similar, ...props }
                                                 </Swiper>
                                             </div>
 
-                                            <div className={classes.tour_topInfo__right___places}>
-                                                {tour.places.map((item, index) => (
-                                                    <div className={classes.tour_topInfo__right___places____place} key={index}>
-                                                        <div className={classes.tour_topInfo__right___places____place_____img}>
-                                                            <svg width="17" height="22" viewBox="0 0 17 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                <path
-                                                                    d="M8.49996 21.5C7.05646 20.2433 5.71847 18.8656 4.49998 17.3814C2.67142 15.1523 0.500001 11.8327 0.500001 8.66931C0.49838 5.36547 2.4477 2.38623 5.43834 1.12183C8.42898 -0.142565 11.8714 0.5571 14.1594 2.89434C15.6639 4.42306 16.5067 6.50255 16.5 8.66931C16.5 11.8327 14.3285 15.1523 12.4999 17.3814C11.2814 18.8656 9.94346 20.2433 8.49996 21.5ZM8.49996 5.17003C7.27506 5.17003 6.1432 5.83699 5.53075 6.91967C4.91829 8.00235 4.91829 9.33627 5.53075 10.419C6.1432 11.5016 7.27506 12.1686 8.49996 12.1686C10.3935 12.1686 11.9285 10.6019 11.9285 8.66931C11.9285 6.73671 10.3935 5.17003 8.49996 5.17003Z"
-                                                                    fill="var(--white_color)"
-                                                                />
-                                                            </svg>
-                                                        </div>
-                                                        <div className={classes.tour_topInfo__right___places____place_____title}>
-                                                            {
-                                                                places ?
-                                                                    places.length != 0 ?
-                                                                        places.map((place, index) => {
-                                                                            const normalizedTitle = place.title.toLowerCase();
-                                                                            const normalizedItem = item.toLowerCase();
-                                                                            if (normalizedTitle.includes(normalizedItem) || normalizedItem.includes(normalizedTitle)) {
-                                                                                return (
-                                                                                    <Link to={`/visits/${place._id}`} key={index}>
-                                                                                        {place.title}
-                                                                                    </Link>
-                                                                                );
-                                                                            } else {
-                                                                                return (
-                                                                                    <span key={index}>
-                                                                                        {item}
-                                                                                    </span>
-                                                                                );
-                                                                            }
-                                                                        })
+                                            <div className={classes.tour_topInfo__right___places_gradient}>
+                                                <div className={classes.tour_topInfo__right___places}>
+                                                    {tour.places.map((item, index) => (
+                                                        <div className={classes.tour_topInfo__right___places____place} key={index}>
+                                                            <div className={classes.tour_topInfo__right___places____place_____img}>
+                                                                <svg width="17" height="22" viewBox="0 0 17 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path
+                                                                        d="M8.49996 21.5C7.05646 20.2433 5.71847 18.8656 4.49998 17.3814C2.67142 15.1523 0.500001 11.8327 0.500001 8.66931C0.49838 5.36547 2.4477 2.38623 5.43834 1.12183C8.42898 -0.142565 11.8714 0.5571 14.1594 2.89434C15.6639 4.42306 16.5067 6.50255 16.5 8.66931C16.5 11.8327 14.3285 15.1523 12.4999 17.3814C11.2814 18.8656 9.94346 20.2433 8.49996 21.5ZM8.49996 5.17003C7.27506 5.17003 6.1432 5.83699 5.53075 6.91967C4.91829 8.00235 4.91829 9.33627 5.53075 10.419C6.1432 11.5016 7.27506 12.1686 8.49996 12.1686C10.3935 12.1686 11.9285 10.6019 11.9285 8.66931C11.9285 6.73671 10.3935 5.17003 8.49996 5.17003Z"
+                                                                        fill="var(--black_color)"
+                                                                    />
+                                                                </svg>
+                                                            </div>
+                                                            <div className={classes.tour_topInfo__right___places____place_____title}>
+                                                                {
+                                                                    places ?
+                                                                        places.length != 0 ?
+                                                                            places.map((place, index) => {
+                                                                                const normalizedTitle = place.title.toLowerCase();
+                                                                                const normalizedItem = item.toLowerCase();
+                                                                                if (normalizedTitle.includes(normalizedItem) || normalizedItem.includes(normalizedTitle)) {
+                                                                                    return (
+                                                                                        <Link to={`/visits/${place._id}`} key={index}>
+                                                                                            {place.title}
+                                                                                        </Link>
+                                                                                    );
+                                                                                } else {
+                                                                                    return (
+                                                                                        <span key={index}>
+                                                                                            {item}
+                                                                                        </span>
+                                                                                    );
+                                                                                }
+                                                                            })
+                                                                            : item
                                                                         : item
-                                                                    : item
-                                                            }
+                                                                }
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                ))}
+                                                    ))}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -195,7 +197,7 @@ function Tours({ children, requestType, pageName, tableName, similar, ...props }
                                         modules={[Navigation]}
                                         slidesPerView={3}
                                         spaceBetween={30}
-                                        className={'similarTours'} 
+                                        className={'similarTours'}
                                         breakpoints={{
                                             320: {
                                                 slidesPerView: 1,
