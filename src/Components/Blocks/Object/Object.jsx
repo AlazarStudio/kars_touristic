4f101e178341c84e4ac782dcbc128c832fb256a6
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-06-26 15:04:39 +0300

    Update

--- src/Components/Blocks/Object/Object.jsx
+++ src/Components/Blocks/Object/Object.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from './Object.module.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
+import server from '../../../serverConfig';
 
-import server from '../../../serverConfig'
-
-function Object({ children, pageName, titleObject, ...props }) {
+function Object({ pageName, titleObject, regionData, width }) {
     function truncateString(str, maxLength) {
         if (str.length > maxLength) {
             return str.substring(0, maxLength) + '...';
@@ -14,12 +13,12 @@ function Object({ children, pageName, titleObject, ...props }) {
 
     let pageNameVisit = '';
 
-    if (props.regionData.days) {
-        if (props.regionData.days.length > 1) {
-            pageNameVisit = 'tours'
+    if (regionData.days) {
+        if (regionData.days.length > 1) {
+            pageNameVisit = 'tours';
         }
-        if (props.regionData.days.length <= 1) {
-            pageNameVisit = 'excursions'
+        if (regionData.days.length <= 1) {
+            pageNameVisit = 'excursions';
         }
     }
 
@@ -30,56 +29,124 @@ function Object({ children, pageName, titleObject, ...props }) {
         });
     }
 
-    return (
-        <>
-            <div className={classes.objects_item} style={{ width: props.width }}>
-                <div className={classes.objects_item__img}>
-                    <img src={`${server}/refs/${props.regionData.mainPhoto ? props.regionData.mainPhoto : props.regionData.photos[0]}`} alt="" />
-                </div>
-                <div className={classes.objects_item__bottom}>
+    const token = localStorage.getItem('token');
 
-                    {titleObject != 'title' ?
+    const updateUser = async (token, updates) => {
+        try {
+            const response = await fetch(`${server}/api/userUpdate`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updates)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to update user.');
+            }
+            // Возвращаем обновленные данные пользователя
+            return await response.json();
+        } catch (error) {
+            console.error('Error updating user:', error);
+            throw error; // Передаем ошибку выше для обработки в компоненте
+        }
+    };
+
+    const getUserInfo = async (token) => {
+        try {
+            const response = await fetch(`${server}/api/user`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            if (response.ok) {
+                return await response.json(); // Возвращаем данные пользователя
+            } else {
+                throw new Error('Failed to fetch user data.');
+            }
+        } catch (error) {
+            console.error('Error fetching user info:', error);
+            throw error; // Передаем ошибку выше для обработки в компоненте
+        }
+    };
+
+    const [user, setUser] = useState(null); // Используем null в качестве начального значения
+
+    useEffect(() => {
+        // Загружаем информацию о пользователе при монтировании компонента
+        getUserInfo(token)
+            .then(userData => setUser(userData))
+            .catch(error => console.error('Error initializing user:', error));
+    }, [token]);
+
+    const navigate = useNavigate();
+    
+    const handleLikeClick = async () => {
+        if (token) {
+            const updates = {
+                likes: [regionData._id]
+            };
+
+            try {
+                const updatedUser = await updateUser(token, updates);
+                setUser(updatedUser); 
+            } catch (error) {
+                console.error('Error updating user:', error);
+            }
+        } else {
+            navigate('/signIn');
+        }
+    };
+
+    return (
+        <div className={classes.objects_item} style={{ width: width }}>
+            <div className={classes.objects_item__like} onClick={handleLikeClick}>
+                {user && user.likes && user.likes.includes(regionData._id)
+                    ? <img src="/userLike_full.png" alt="Liked" />
+                    : <img src="/userLike_empty.png" alt="Not Liked" />
+                }
+            </div>
+            <div className={classes.objects_item__img}>
+                <img src={`${server}/refs/${regionData.mainPhoto ? regionData.mainPhoto : regionData.photos[0]}`} alt="" />
+            </div>
+            <div className={classes.objects_item__bottom}>
+                {titleObject !== 'title'
+                    ? (
                         <>
                             <div className={classes.objects_item__title}>
-                                {truncateString(props.regionData[`${titleObject}`], 50)}
+                                {truncateString(regionData[titleObject], 50)}
                             </div>
                             <div className={classes.objects_item__desc}>
-                                <div>Cпособ передвижения: <span>{props.regionData[`travelMethod`]}</span></div>
-                                <div>Продолжительность: <span>{props.regionData[`duration`]}</span></div>
-                                <div>Время отправления: <span>{props.regionData[`departureTime`]}</span></div>
-                                <div>Тип экскурсии: <span>{props.regionData[`tourType`]}</span></div>
-                                <div>Сложность: <span>{props.regionData[`difficulty`]}</span></div>
+                                <div>Cпособ передвижения: <span>{regionData.travelMethod}</span></div>
+                                <div>Продолжительность: <span>{regionData.duration}</span></div>
+                                <div>Время отправления: <span>{regionData.departureTime}</span></div>
+                                <div>Тип экскурсии: <span>{regionData.tourType}</span></div>
+                                <div>Сложность: <span>{regionData.difficulty}</span></div>
                             </div>
-                            
-                            {props.regionData[`optional`] ?
+                            {regionData.optional &&
                                 <div className={classes.objects_item__optional}>
-                                    <img src="/optional.png" alt="" /> {props.regionData[`optional`]}
+                                    <img src="/optional.png" alt="Optional" /> {regionData.optional}
                                 </div>
-                                : null
                             }
-
                             <div className={classes.objects_item__price}>
-                                <div>Стоимость: <span>{props.regionData[`cost`]} ₽ *</span></div>
+                                <div>Стоимость: <span>{regionData.cost} ₽ *</span></div>
                             </div>
-
                             <div className={classes.buttons}>
-                                <Link to={`/${pageName ? pageName : pageNameVisit}/${props.regionData._id}`} className={classes.objects_item__button} onClick={toTop}>Подробнее</Link>
+                                <Link to={`/${pageName ? pageName : pageNameVisit}/${regionData._id}`} className={classes.objects_item__button} onClick={toTop}>Подробнее</Link>
                                 <Link to={``} className={`${classes.objects_item__button} ${classes.objects_item__bron}`} >Забронировать</Link>
                             </div>
                         </>
-                        :
-                        <>
-                            <div className={classes.objects_item__title} style={{ textAlign: 'center' }}>
-                                {truncateString(props.regionData[`${titleObject}`], 50)}
-                            </div>
-                            <Link to={`/${pageName ? pageName : pageNameVisit}/${props.regionData._id}`} className={classes.objects_item__button} onClick={toTop}>Подробнее</Link>
-                        </>
-                    }
-
-
-                </div>
+                    )
+                    : (
+                        <div className={classes.objects_item__title} style={{ textAlign: 'center' }}>
+                            {truncateString(regionData[titleObject], 50)}
+                        </div>
+                    )
+                }
             </div>
-        </>
+        </div>
     );
 }
 
