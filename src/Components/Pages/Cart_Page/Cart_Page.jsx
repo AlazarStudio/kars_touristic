cf30c77bf1821cf81d2df65c74f5a323fe8733ff
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-08-19 13:53:01 +0300

    Update

--- src/Components/Pages/Cart_Page/Cart_Page.jsx
+++ src/Components/Pages/Cart_Page/Cart_Page.jsx
@@ -10,6 +10,11 @@ function Cart_Page({ children, ...props }) {
     const [multidayTours, setMultidayTours] = useState([]);
     const [onedayTours, setOnedayTours] = useState([]);
     const [selectedTours, setSelectedTours] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [passengerCount, setPassengerCount] = useState(1);
+    const [passengerInfo, setPassengerInfo] = useState([]);
+    const [paymentMethod, setPaymentMethod] = useState("card");
+    const [isAgreed, setIsAgreed] = useState(false);
 
     const token = localStorage.getItem('token');
 
@@ -83,6 +88,41 @@ function Cart_Page({ children, ...props }) {
         });
     };
 
+    const handlePlaceOrder = () => {
+        if (user.role === "agent") {
+            setIsModalOpen(true);
+        } else {
+            // обычный процесс оформления заказа
+        }
+    };
+
+    const handlePassengerCountChange = (e) => {
+        const count = parseInt(e.target.value);
+        setPassengerCount(count);
+
+        const newPassengerInfo = Array.from({ length: count }, (_, i) => ({
+            fullName: "",
+            email: "",
+            phone: "",
+        }));
+        setPassengerInfo(newPassengerInfo);
+    };
+
+    const handlePassengerInfoChange = (index, field, value) => {
+        const updatedPassengerInfo = [...passengerInfo];
+        updatedPassengerInfo[index][field] = value;
+        setPassengerInfo(updatedPassengerInfo);
+    };
+
+    const handleBooking = () => {
+        const totalSum = totalCost * passengerCount;
+        console.log("Total sum:", totalSum);
+        console.log("Passenger info:", passengerInfo);
+        console.log("Payment method:", paymentMethod);
+
+        // Далее можно отправить данные на сервер для завершения бронирования
+    };
+
     let cartMass = user ? user.cart : [];
 
     const filteredMultidayTours = multidayTours.length > 0 ? multidayTours.filter(tour => cartMass.includes(tour._id)) : [];
@@ -95,6 +135,15 @@ function Cart_Page({ children, ...props }) {
     const formatNumber = (num) => {
         return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     };
+
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [isModalOpen]);
+    
     return (
         <>
             <Header_black />
@@ -115,19 +164,17 @@ function Cart_Page({ children, ...props }) {
                                                     onChange={() => handleSelectTour(item)}
                                                 />
                                                 <div className={classes.cartPage_left__data___elem____objectData}>
-                                                    <div className={classes.cartPage_left__data___elem____objectData___info}>
-                                                        <div className={classes.objectData_img}>
-                                                            <img src={`${server}/refs/${item.mainPhoto}`} alt="" />
-                                                        </div>
-                                                        <div className={classes.objectData_info}>
-                                                            <div className={classes.objectData_info__title}>{item.tourTitle}</div>
-                                                            <div className={classes.objectData_info__list}>
-                                                                <p>Способ передвижения: <span>{item.travelMethod}</span></p>
-                                                                <p>Продолжительность: <span>{item.duration}</span></p>
-                                                                <p>Время отправления: <span>{item.departureTime}</span></p>
-                                                                <p>Тип экскурсии: <span>{item.tourType}</span></p>
-                                                                <p>Сложность: <span>{item.difficulty}</span></p>
-                                                            </div>
+                                                    <div className={classes.objectData_img}>
+                                                        <img src={`${server}/refs/${item.mainPhoto}`} alt="" />
+                                                    </div>
+                                                    <div className={classes.objectData_info}>
+                                                        <div className={classes.objectData_info__title}>{item.tourTitle}</div>
+                                                        <div className={classes.objectData_info__list}>
+                                                            <p>Способ передвижения: <span>{item.travelMethod}</span></p>
+                                                            <p>Продолжительность: <span>{item.duration}</span></p>
+                                                            <p>Время отправления: <span>{item.departureTime}</span></p>
+                                                            <p>Тип экскурсии: <span>{item.tourType}</span></p>
+                                                            <p>Сложность: <span>{item.difficulty}</span></p>
                                                         </div>
                                                     </div>
                                                     <div className={classes.objectData_price}>
@@ -156,18 +203,126 @@ function Cart_Page({ children, ...props }) {
                                         <div className={classes.cartPage_right__info___lower___num}>{data.length}</div>
                                     </div>
                                 </div>
-                                <div className={classes.cartPage_right__button}>Оформить заказ</div>
-                                <div className={classes.cartPage_right__oferta}>
-                                    <label>
-                                        <input type="radio" />
-                                        <p>Согласен с условиями Правил пользования торговой площадкой и правилами возврата</p>
-                                    </label>
+                                <div className={classes.cartPage_right__button}
+                                    onClick={selectedTours.length > 0 ? handlePlaceOrder : null}
+                                    style={{
+                                        backgroundColor: selectedTours.length > 0 ? '#007bff' : '#ccc',
+                                        cursor: selectedTours.length > 0 ? 'pointer' : 'not-allowed'
+                                    }}>
+                                    Оформить заказ
                                 </div>
+                                {selectedTours.length === 0 && (
+                                    <div className={classes.errorMessage}>
+                                        Пожалуйста, выберите тур перед оформлением заказа.
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
                 </WidthBlock>
             </CenterBlock>
+
+            {isModalOpen && (
+                <div className={classes.modal}>
+                    <div className={classes.modalContent}>
+                        <div className={classes.closeButton} onClick={() => {
+                            setIsModalOpen(false);
+                            setPassengerInfo([]);
+                            setPassengerCount(1)
+                        }}>
+                            &times;
+                        </div>
+                        <h2>Введите количество людей</h2>
+
+                        <input
+                            type="number"
+                            min="1"
+                            value={passengerCount}
+                            onChange={handlePassengerCountChange}
+                            className={classes.passangerCount}
+                        />
+
+                        <div className={classes.passengerBlock}>
+                            {Array.from({ length: passengerCount }, (_, i) => (
+                                <div key={i} className={classes.passengerInfo}>
+                                    <h3>Пассажир {i + 1}</h3>
+                                    <input
+                                        type="text"
+                                        placeholder="ФИО"
+                                        value={passengerInfo[i]?.fullName || ""}
+                                        onChange={(e) => handlePassengerInfoChange(i, "fullName", e.target.value)}
+                                    />
+                                    <input
+                                        type="email"
+                                        placeholder="Почта"
+                                        value={passengerInfo[i]?.email || ""}
+                                        onChange={(e) => handlePassengerInfoChange(i, "email", e.target.value)}
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="Номер телефона"
+                                        value={passengerInfo[i]?.phone || ""}
+                                        onChange={(e) => handlePassengerInfoChange(i, "phone", e.target.value)}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className={classes.totalSum}>
+                            Итоговая сумма: {formatNumber(totalCost * passengerCount)} ₽
+                        </div>
+
+                        <div className={classes.paymentMethod}>
+                            <h3>Выберите способ оплаты</h3>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="card"
+                                    checked={paymentMethod === "card"}
+                                    onChange={(e) => setPaymentMethod(e.target.value)}
+                                />
+                                Карта
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    value="cash"
+                                    checked={paymentMethod === "cash"}
+                                    onChange={(e) => setPaymentMethod(e.target.value)}
+                                />
+                                Наличные
+                            </label>
+                        </div>
+
+                        <br />
+                        <br />
+                        <br />
+
+                        <div className={classes.cartPage_right__oferta}>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    checked={isAgreed}
+                                    onChange={(e) => setIsAgreed(e.target.checked)}
+                                    required
+                                />
+                                <p>Согласен с условиями Правил пользования торговой площадкой и правилами возврата</p>
+                            </label>
+                            <button
+                                onClick={isAgreed ? handleBooking : null}
+                                disabled={!isAgreed}
+                                style={{
+                                    backgroundColor: isAgreed ? '#007bff' : '#ccc',
+                                    cursor: isAgreed ? 'pointer' : 'not-allowed'
+                                }}
+                            >
+                                Забронировать тур
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            )}
         </>
     );
 }
