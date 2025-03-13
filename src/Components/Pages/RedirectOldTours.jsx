01163990d365c4a284e6b3c97b45f8c4d61c762a
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-03-13 18:12:20 +0300

    Update redirect localStorage

--- src/Components/Pages/RedirectOldTours.jsx
+++ src/Components/Pages/RedirectOldTours.jsx
@@ -15,28 +15,36 @@ function RedirectOldTours({ children, ...props }) {
     let urlName = getSectionFromUrl();
 
     let requestType
+    let activeTab
 
     switch (urlName) {
         case "tours":
             requestType = 'getOneMultidayTour'
+            activeTab = 1
             break;
         case "excursions":
             requestType = 'getOneOnedayTour'
+            activeTab = 2
             break;
         case "gids":
             requestType = 'getOneAuthorTours'
+            activeTab = 3
             break;
         case "hotels":
             requestType = 'getOneHotel'
+            activeTab = 4
             break;
         case "rooms":
             requestType = 'getOneRoom'
+            activeTab = 4
             break;
         case "visits":
             requestType = 'getOnePlace'
+            activeTab = 5
             break;
         case "events":
             requestType = 'getOneEvent'
+            activeTab = 6
             break;
         default:
             requestType = "Страница не найдена";
@@ -56,6 +64,7 @@ function RedirectOldTours({ children, ...props }) {
     }, [id]);
 
     const navigate = useNavigate();
+    localStorage.setItem("activeTab", activeTab.toString());
 
     if (tour) {
         navigate(`/region/${tour.region}/${id}`);
